7231b235f0846a1c1a4479f7383fa4d38cc611c0
Mariya Efremova <mariyaefremova22@gmail.com>
2022-07-06 17:31:22 +0300

    Add a new task to the to-do list and save the changes to LocalStorage

--- js/index.js
+++ js/index.js
@@ -1,40 +1,63 @@
 const doings = document.querySelector('.input-window');
 const listElement = document.querySelector('li');
+// const listElement = document.querySelector('ul');
+const pagination = document.querySelector('footer');
+const filterBlock = document.querySelector('.filter');
 
-const doingsList = [];
+let doingsList = [];
+const size = 4;
+let currentPage = 1;
+
+if (localStorage.getItem('doingsList')) {
+    doingsList = JSON.parse(localStorage.getItem('doingsList'));
+    displayDoing ();
+};
 
 document.addEventListener('keypress', (event) => {
     if (event.key === 'Enter' && doings.value != '') {
-        
         doingsList.push(doings.value);
 
-        doings.value = '';
-
         displayDoing ();
+
+        doings.value = '';
 
         localStorage.setItem('doingsList', JSON.stringify(doingsList));
     }
 });
 
 function displayDoing () {
-    let contentElement = '';
-    doingsList.forEach((item) => {
-        contentElement += `
+    let doingsElements = '';
+    doingsList.forEach((item) => {{
+        doingsElements += `
             <div class="element-of-list">
                 <div class="element-content">
                     <input type="checkbox" class="checked-doings" id="checkable" />
-                    <label for="checkable">${item}
-                        <button class="delete-doing">
-                        <img
-                            src="../img/2931151_close_delete_exit_remove_x_icon.png"
-                            alt="delete"
-                        />
-                        </button>
-                    </label>
+                    <label for="checkable">${item}</label>
                 </div>
-            </div>`; 
-    })
+                <button class="delete-doing"></button>
+            </div>`;
+        }
+    }); 
 
-    listElement.innerHTML = contentElement; 
+    listElement.innerHTML = doingsElements;
+
+    if (doingsList.length > 3) {
+        pagination.innerHTML = `
+        <div class="pagination">
+        <a href="#">&laquo;</a>
+        <a href="#">1</a>
+        <a href="#secondPage">2</a>
+        <a href="#">&raquo;</a>
+        </div>
+        `
+    };
+
+    if (doingsList.length > 0) {
+        filterBlock.innerHTML = `
+            <button class="allElements">All</button>
+            <button class="doneElements">Done</button>
+            <button class="undoneElements">Undone</button>
+        `
+    };
 };
 
